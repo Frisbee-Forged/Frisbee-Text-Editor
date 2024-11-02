3b39eb093e2c86e6dc18b27de8181191634b9050
The Reliable Dino <youssefachgs@gmail.com>
2024-11-02 18:01:56 +0100

    0.1.3

---
+++ custom-elements/update.js
@@ -0,0 +1,18 @@
+async function checkForUpdates(currentVersion) {
+    const url = `https://api.github.com/repos/TotallyReliableDino/Frisbee-Text-Editor/releases`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const latestRelease = data[0];
+        const latestVersion = latestRelease.tag_name;
+        if (currentVersion < latestVersion) {
+            alert(`A new version (${latestVersion}) is available! You’re using ${currentVersion}. Please update.`);
+        }
+    } catch (error) {
+        console.error("Error checking for updates:", error);
+    }
+}
+
+const currentVersion = "0.1.3d";
+checkForUpdates(currentVersion);
