b4def0320aa0f81f3094c00e16c55b4bc3c7b0a9
The Reliable Dino <youssefachgs@gmail.com>
2024-11-16 12:48:40 +0100

    Update update.js

--- custom-elements/update.js
+++ custom-elements/update.js
@@ -1,28 +1,9 @@
-async function checkForUpdates(currentVersion) {
-    const url = `https://api.github.com/repos/TotallyReliableDino/Frisbee-Text-Editor/releases`;
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        const latestRelease = data[0];
-        const latestVersion = latestRelease.tag_name;
-        if (currentVersion < latestVersion) {
-            alert(`A new version (${latestVersion}) is available! You’re using ${currentVersion}. Please update.`);
-        }
-    } catch (error) {
-        console.error("Error checking for updates:", error);
-    }
-}
-
-function updatetitle(currentVersion) {
+function updatetitle(versionName) {
  subtitle = document.getElementById('subtitle')
   
-  document.title = "Frisbee " + currentVersion;
-  subtitle.textContent = subtitle.textContent + currentVersion;
+  document.title = "Frisbee " + versionName;
+  subtitle.textContent = subtitle.textContent + versionName;
  
 }
-
-
-const currentVersion = "0.1.5u";
-updatetitle(currentVersion);
-checkForUpdates(currentVersion);
+const versionName = "(Github Pages)";
+updatetitle(versionName);
